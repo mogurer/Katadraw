3e5f155dd6093951c853df8d82ec0cdb64e98ba8
mogurer <krtek.mogurer@gmail.com>
2026-04-29 22:37:48 +0900

    体験版Ver.1.0

--- steam_manager.gd
+++ steam_manager.gd
@@ -2,11 +2,11 @@
 extends Node
 
 func _ready():
-    var init = Steam.steamInitEx()
-    if init["status"] != Steam.STEAM_API_INIT_RESULT_OK:
-        print("Steam初期化失敗: ", init)
-    else:
-        print("Steam初期化OK")
+	var init = Steam.steamInitEx()
+	if init["status"] != Steam.STEAM_API_INIT_RESULT_OK:
+		print("Steam初期化失敗: ", init)
+	else:
+		print("Steam初期化OK")
 
 func _process(_delta):
-    Steam.run_callbacks()  # 毎フレーム必須
+	Steam.run_callbacks()  # 毎フレーム必須
